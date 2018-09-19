a54c38447959eebaf47874d7e173e6146634283d
shivksah-aricent <43410873+shivksah-aricent@users.noreply.github.com>
2018-09-19 19:23:48 +0530

    Update HelloWorldTest.php

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -23,7 +23,7 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
     {
         $helloWorld = new HelloWorld($this->pdo);
 
-        $this->assertEquals('Hello World', $helloWorld->hello());
+        $this->assertEquals('Hello CI World', $helloWorld->hello());
     }
 
     public function testHello()
